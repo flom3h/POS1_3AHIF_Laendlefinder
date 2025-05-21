db36d1c092aba84dc9c76fc96b7ffeda45af4e90
Florian Pieles <126450255+flom3h@users.noreply.github.com>
2025-05-21 12:11:15 +0200

    New WPF Project
    C# Projects suck

--- src/frontend-wpf/App.xaml.cs
+++ src/frontend-wpf/App.xaml.cs
@@ -2,11 +2,13 @@
 using System.Data;
 using System.Windows;
 
-namespace Laendlefinder;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace Laendlefinder
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
 }
