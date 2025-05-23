2465a10a642afbb309a313424cb4e21a759c68e3
rohatli11 <rohat.sahbaz@gmail.com>
2025-05-23 10:44:09 +0200

    events abrufen funktioernt nicht

--- src/backend/swagger_server/__main__.py
+++ src/backend/swagger_server/__main__.py
@@ -7,13 +7,13 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 
 load_dotenv()
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
 
+supabase: Client = create_client(url, key)
 def main():
 
-    url: str = os.getenv("SUPABASE_URL")
-    key: str = os.getenv("SUPABASE_KEY")
-
-    supabase: Client = create_client(url, key)
+    
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Events API'}, pythonic_params=True)
