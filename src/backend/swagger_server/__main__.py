db7e7b044cc5a9fe6bfebaac1cf6b56c74284187
rohatli11 <rohat.sahbaz@gmail.com>
2025-05-23 09:47:54 +0200

    verbindung mit superbase

--- src/backend/swagger_server/__main__.py
+++ src/backend/swagger_server/__main__.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
 import connexion
-import sqlite3
 from swagger_server import encoder
+from supabase import create_client, Client
 
+SUPABASE_URL = "https://esgeugesyqlszsalokzz.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZ2V1Z2VzeXFsc3pzYWxva3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NzY0ODEsImV4cCI6MjA2MjM1MjQ4MX0.e1zRQahAM85oPyJv6wFv91JMMBQ6BGersv8H632mMI4"
+supabase: Client = None
 
 def init_db():
-    pass
-    
+    global supabase
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+    print("Verbindung zur Supabase-Datenbank hergestellt!")
 
 def main():
+    init_db()
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Events API'}, pythonic_params=True)
     app.run(port=8080)
-
 
 if __name__ == '__main__':
     main()
