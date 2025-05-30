ade3fb0a0b08f0c3b9f1a530c976747cd79d438d
EdgeLordJ <joudi.khalil@student.htl-rankweil.at>
2025-05-30 14:46:23 +0200

    Login Design vollendet; Passwort visibility change erfolg; Vordefinierte Styles für Button und Textboxen für Login/Register gemacht

--- src/frontend-wpf/Pages/RegisterPage.xaml.cs
+++ src/frontend-wpf/Pages/RegisterPage.xaml.cs
@@ -1,11 +1,38 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Laendlefinder.Pages;
 
 public partial class RegisterPage : Page
 {
+    private bool passwordVisible = false;
     public RegisterPage()
     {
         InitializeComponent();
+        PasswordBox.Visibility = Visibility.Visible;
+        PlainPasswordBox.Visibility = Visibility.Collapsed;
+    }
+
+    private void ChangePasswordVisibility_Click(object sender, RoutedEventArgs e)
+    {
+        passwordVisible = !passwordVisible;
+
+        if (passwordVisible)
+        {
+            PlainPasswordBox.Text = PasswordBox.Password;
+            PasswordBox.Visibility = Visibility.Collapsed;
+            PlainPasswordBox.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            PasswordBox.Password = PlainPasswordBox.Text;
+            PasswordBox.Visibility = Visibility.Visible;
+            PlainPasswordBox.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    private void RegButton_Click(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
