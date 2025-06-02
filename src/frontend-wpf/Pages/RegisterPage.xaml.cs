90614ea0214cd1ec29d3bf0ce596d0550ac5f10c
EdgeLordJ <joudi.khalil@student.htl-rankweil.at>
2025-06-02 14:28:33 +0200

    RegisterPage Design verbessert; Placeholder für Textboxen und Passwordbox hinzugefügt

--- src/frontend-wpf/Pages/RegisterPage.xaml.cs
+++ src/frontend-wpf/Pages/RegisterPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Laendlefinder.Pages;
 
@@ -34,5 +35,30 @@ public partial class RegisterPage : Page
     private void RegButton_Click(object sender, RoutedEventArgs e)
     {
         throw new NotImplementedException();
+    }
+
+    private void SnBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        PlaceholderSn.Visibility = string.IsNullOrEmpty(SnBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    private void LnBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        PlaceholderLn.Visibility = string.IsNullOrEmpty(LnBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    private void MailBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        PlaceholderMail.Visibility = string.IsNullOrEmpty(MailBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    private void PlainPasswordBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        PlaceholderPassword.Visibility = string.IsNullOrEmpty(PlainPasswordBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        PlaceholderPassword.Visibility = string.IsNullOrEmpty(PasswordBox.Password) ? Visibility.Visible : Visibility.Collapsed;
     }
 }
