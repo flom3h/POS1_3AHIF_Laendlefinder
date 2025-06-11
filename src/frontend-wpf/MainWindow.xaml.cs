845bab4df42fbbdd0ffb09e0a946a9cf97c42cfe
EdgeLordJ <joudi.khalil@student.htl-rankweil.at>
2025-06-11 12:51:08 +0200

    Navigation wieder erstellt, weil irgendwie gelöscht worden; ProfilePage Design erstellt fehlt Logik plus Endpunkt in Backend

--- src/frontend-wpf/MainWindow.xaml.cs
+++ src/frontend-wpf/MainWindow.xaml.cs
@@ -27,6 +27,29 @@ namespace Laendlefinder
 
             RegisterPage.LoginButtonClickedNavLogin += DisplayLogin;
             RegisterPage.LoginButtonClickedNavHome += DisplayHome;
+            
+            ProfilePage.HomeButtonClickedNavHome += DisplayHome;
+            ProfilePage.ExploreButtonClickedNavExplore += DisplayExplore;
+            ProfilePage.CalendarButtonClickedNavCalendar += DisplayCalendar;
+            ProfilePage.FavsButtonClickedNavFavs += DisplayFavorites;
+            ProfilePage.MapButtonClickedNavMap += DisplayMap;
+            ProfilePage.ProfileButtonClickedNavProfile += DisplayProfile;
+            ProfilePage.SaveChangesButtonClickedNavHome += DisplayHome;
+            ProfilePage.LogOutButtonClickedNavLogin += DisplayLogin;
+            
+            ExplorePage.HomeButtonClickedNavHome += DisplayHome;
+            ExplorePage.ExploreButtonClickedNavExplore += DisplayExplore;
+            ExplorePage.CalendarButtonClickedNavCalendar += DisplayCalendar;
+            ExplorePage.FavsButtonClickedNavFavs += DisplayFavorites;
+            ExplorePage.MapButtonClickedNavMap += DisplayMap;
+            ExplorePage.ProfileButtonClickedNavProfile += DisplayProfile;
+            
+            FavoritesPage.HomeButtonClickedNavHome += DisplayHome;
+            FavoritesPage.ExploreButtonClickedNavExplore += DisplayExplore;
+            FavoritesPage.CalendarButtonClickedNavCalendar += DisplayCalendar;
+            FavoritesPage.FavsButtonClickedNavFavs += DisplayFavorites;
+            FavoritesPage.MapButtonClickedNavMap += DisplayMap;
+            FavoritesPage.ProfileButtonClickedNavProfile += DisplayProfile;
         }
         
         private void DisplayHome (object sender, System.EventArgs e)
@@ -44,6 +67,36 @@ namespace Laendlefinder
         private void DisplayRegister (object sender, System.EventArgs e)
         {
             RegisterPage mainPage = new RegisterPage();
+            MainFrame.Navigate(mainPage);
+        }
+        
+        private void DisplayCalendar (object sender, System.EventArgs e)
+        {
+            CalendarPage mainPage = new CalendarPage();
+            MainFrame.Navigate(mainPage);
+        }
+        
+        private void DisplayExplore (object sender, System.EventArgs e)
+        {
+            ExplorePage mainPage = new ExplorePage();
+            MainFrame.Navigate(mainPage);
+        }
+        
+        private void DisplayProfile (object sender, System.EventArgs e)
+        {
+            ProfilePage mainPage = new ProfilePage();
+            MainFrame.Navigate(mainPage);
+        }
+        
+        private void DisplayFavorites (object sender, System.EventArgs e)
+        {
+            FavoritesPage mainPage = new FavoritesPage();
+            MainFrame.Navigate(mainPage);
+        }
+        
+        private void DisplayMap (object sender, System.EventArgs e)
+        {
+            MapPage mainPage = new MapPage();
             MainFrame.Navigate(mainPage);
         }
     }
